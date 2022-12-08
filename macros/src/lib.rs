486c94d443d8736578343b4348278c18f1d40f7b
alberrttt <bertbocks@gmail.com>
2022-12-08 11:11:17 -0800

    aaa

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(proc_macro_quote)]
 use std::{
     env,
     fs::{self, read_to_string},
@@ -5,7 +6,7 @@ use std::{
 };
 
 use proc_macro::{self, TokenStream};
-
+use quote::quote;
 #[proc_macro]
 pub fn make_tests(_item: TokenStream) -> TokenStream {
     let mut stream = TokenStream::new();
@@ -16,37 +17,35 @@ pub fn make_tests(_item: TokenStream) -> TokenStream {
     recurse_dir(path.as_path(), &mut stream, String::new());
     stream
 }
-fn recurse_dir(path: &Path, stream: &mut TokenStream, post_pend: String) {
+fn recurse_dir(path: &Path, stream: &mut TokenStream, pre_pend: String) {
+    let mut test_gen = Vec::new();
     for (i, file) in fs::read_dir(path).unwrap().enumerate() {
         let file = file.unwrap();
         if file.file_type().unwrap().is_file() {
-            let path = file.path().as_path().to_string_lossy().to_string();
-            let t: TokenStream = format!(
-                "
-#[test]
-fn {}{}() {{
-use rottenmangos::{{
-    backend::vm::VM,
-    cli_context::Context,
-    frontend::compiler::{{Compiler, FunctionType}},
-}};
-use std::path::Path;
-use std::fs::read_to_string;
-let source = read_to_string(Path::new(r#\"{path}\"#)).unwrap();
-let mut context = Context::new(Path::new(r#\"{path}\"#));
-let compiler = Compiler::new(&mut context, FunctionType::Script);
-let mut vm = VM::new();
+            let path = file.path();
+            let name = path.file_stem().unwrap().to_str().unwrap();
+            let name = pre_pend.to_owned() + name;
+            let token = quote! {
+                #[test]
+                fn #name{
+                    use rottenmangos::{
+                   backend::vm::VM,
+                    cli_context::Context,
+                    frontend::compiler::{{Compiler, FunctionType}},
+                };
+                use std::path::Path;
+                use std::fs::read_to_string;
+                let source = read_to_string(Path::new("{path}")).unwrap();
+                let mut context = Context::new(Path::new("{path}"));
+                let compiler = Compiler::new(&mut context, FunctionType::Script);
+                let mut vm = VM::new();
 
-vm.run(compiler.compile(source).unwrap().chunk);
-}}\n",
-                &post_pend,
-                file.path().file_stem().unwrap().to_str().unwrap(),
-            )
-            .parse()
-            .unwrap();
-            stream.extend(t.into_iter());
+                vm.run(compiler.compile(source).unwrap().chunk);
+                }
+            };
+            test_gen.push(token)
         } else {
-            let mut tmp = post_pend.clone();
+            let mut tmp = pre_pend.clone();
             tmp.push_str(
                 &(file
                     .path()
@@ -58,6 +57,6 @@ vm.run(compiler.compile(source).unwrap().chunk);
                     + "_"),
             );
             recurse_dir(file.path().as_path(), stream, tmp);
-        }
+        };
     }
 }
